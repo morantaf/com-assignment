7855b3524ecfe8044606d233ad8f720f71de7d78
morantaf <fall_moranta@live.fr>
2020-10-08 10:32:08 +0200

    Create Recipe Domain Class

---
+++ grails-app/domain/com/assignment/Recipe.groovy
@@ -0,0 +1,12 @@
+package com.assignment
+
+class Recipe {
+
+    String name // name of the recipe
+    String description // Description of the recipe
+    int timeInMinutes // Time to cook
+    static ArrayList<String> meal = ["Breakfast","Lunch","Dinner"]
+
+    static constraints = {
+    }
+}
---
+++ src/test/groovy/com/assignment/RecipeSpec.groovy
@@ -0,0 +1,16 @@
+package com.assignment
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class RecipeSpec extends Specification implements DomainUnitTest<Recipe> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
