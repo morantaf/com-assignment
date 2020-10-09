2628fcfe87217e430cb954caba7d4b8aa6334e8f
morantaf <fall_moranta@live.fr>
2020-10-09 14:07:29 +0200

    deleting unused method and adding comments

--- grails-app/services/com/assignment/RecipeService.groovy
+++ grails-app/services/com/assignment/RecipeService.groovy
@@ -5,19 +5,19 @@ import grails.gorm.transactions.Transactional
 @Transactional
 class RecipeService {
 
+    // Logic to retrieve a Recipe
     def get(id) {
         Recipe.get(id)
     }
 
+    // Logic to retrieve the list of recipes
     def list() {
         Recipe.list()
     }
 
+    // Logic to save the recipe
     def save(recipe) {
         recipe.save()
     }
 
-    def serviceMethod() {
-
-    }
 }
