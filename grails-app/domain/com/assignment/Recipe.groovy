a4fb4d06d63aa8bd9d8ca0c37d818011926a0f67
morantaf <fall_moranta@live.fr>
2020-10-09 14:06:35 +0200

    Adding id and constraints

--- grails-app/domain/com/assignment/Recipe.groovy
+++ grails-app/domain/com/assignment/Recipe.groovy
@@ -4,6 +4,7 @@ import enums.Meal
 
 class Recipe {
 
+    Long id // id of the instance in the database
     String name // name of the recipe
     String description // Description of the recipe
     int timeInMinutes // Time required to cook in minutes
@@ -11,6 +12,10 @@ class Recipe {
 
     static hasMany = [ingredients : Ingredient]
 
+    // Add constraint for the timeInMinutes so that it can't go negative
     static constraints = {
+        name(matches: "[a-zA-Z\\s]+")
+        description(matches: "[a-zA-Z\\s]+")
+        timeInMinutes(min:0)
     }
 }
