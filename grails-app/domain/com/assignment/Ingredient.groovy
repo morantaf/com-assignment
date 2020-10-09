a5b85c32551c77604b05825fcc2d40ad2c9738ad
morantaf <fall_moranta@live.fr>
2020-10-09 14:02:15 +0200

    Adding the missing arguments and the constraints

--- grails-app/domain/com/assignment/Ingredient.groovy
+++ grails-app/domain/com/assignment/Ingredient.groovy
@@ -4,12 +4,17 @@ import enums.AmountType
 
 class Ingredient {
 
+    Long id // id of the instance in the database
     String name // Name of the ingredient
-    AmountType amount  // Amount required
+    int amount  // Amount required
+    AmountType amountType // To choose the type of Amount between Grams, Milliliters etc...
 
 
     static belongsTo = [Recipe]
 
+    // Constraint to prevent the use of negative input for the time
     static constraints = {
+        name(matches: "[a-zA-Z\\s]+")
+        amount(min:0)
     }
 }
