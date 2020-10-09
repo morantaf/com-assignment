2cb62dc96baa8b665956b14b91f482fed3daa80d
morantaf <fall_moranta@live.fr>
2020-10-09 14:07:07 +0200

    updating allowedMethods and adding comments

--- grails-app/controllers/com/assignment/RecipeController.groovy
+++ grails-app/controllers/com/assignment/RecipeController.groovy
@@ -1,33 +1,36 @@
 package com.assignment
 
-import grails.validation.ValidationException
-import static org.springframework.http.HttpStatus.*
-
 class RecipeController {
 
     RecipeService recipeService
 
-    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+    static allowedMethods = [save: "POST", update: ["PUT","GET", 'POST'], delete: "DELETE"]
 
+    // Action to display a list of Recipe
     def index() {
         respond recipeService.list()
     }
 
+    // Action to display the details of one recipe
     def show(Long id) {
         respond recipeService.get(id)
     }
 
+    // Action to display the form to create a new instance of Recipe
     def create() {
         respond new Recipe(params)
     }
 
+    // Action to save the instance of recipe created or updated
     def save(Recipe recipe) {
         recipeService.save(recipe)
         redirect action:"index", method:"GET"
     }
 
+    // Action to display the form to update an instance of Recipe
     def update(Long id) {
         respond recipeService.get(id)
+
     }
 
 
