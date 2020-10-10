bb3a12cf81a8b0715f7a34838f63fa6c6d285c50
morantaf <fall_moranta@live.fr>
2020-10-10 13:40:21 +0200

    Adding get and creatIngredient logics

--- grails-app/services/com/assignment/IngredientService.groovy
+++ grails-app/services/com/assignment/IngredientService.groovy
@@ -1,7 +1,7 @@
 package com.assignment
 
+import enums.AmountType
 import grails.gorm.transactions.Transactional
-import grails.web.RequestParameter
 
 @Transactional
 class IngredientService {
@@ -10,6 +10,21 @@ class IngredientService {
     //Logic to save an ingredient
     def save(ingredient) {
         ingredient.save()
+    }
+
+    // Logic to retrieve an ingredient
+    def get(id) {
+        Ingredient.get(id)
+    }
+
+    // Logic to create an ingredient and link it to a recipe
+    def createIngredient(String recipeId, String name, int amount, String type){
+        def recipe = Recipe.findById(recipeId)
+        if(recipe) {
+            def ingredient = new Ingredient(name: name, amount: amount, type : type)
+            recipe.addToIngredients(ingredient).save()
+            return ingredient
+        }
     }
 
 }
