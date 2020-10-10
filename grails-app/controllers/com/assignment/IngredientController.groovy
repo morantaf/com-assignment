d38f34cc6ab22b4f6d33bc244886b94c46ead3f2
morantaf <fall_moranta@live.fr>
2020-10-10 13:39:56 +0200

    Adding addIngredients to allowedMethods and adding edit, show and addIngredient actions

--- grails-app/controllers/com/assignment/IngredientController.groovy
+++ grails-app/controllers/com/assignment/IngredientController.groovy
@@ -4,11 +4,21 @@ class IngredientController {
 
     IngredientService ingredientService
 
-    static allowedMethods = [save:"POST"]
+    static allowedMethods = [save:"POST", edit:"", addIngredient: "POST"]
 
     // Action to display a form to create an instance of Ingredient
     def create() {
         respond new Ingredient(params)
+    }
+
+    // Action to display the form to edit an ingredient
+    def edit(Long id) {
+        respond ingredientService.get(id)
+    }
+
+    // Action to display one ingredient
+    def show(Long id) {
+        respond ingredientService.get(id)
     }
 
     // Action to save a created ingredient
@@ -16,5 +26,14 @@ class IngredientController {
         ingredientService.save(ingredient)
         redirect(controller:"recipe", action:"index", method:"GET")
     }
+
+    // Action to add an Ingredient to a recipe
+    def addIngredient(String id, String name, int amount, String type) {
+        ingredientService.createIngredient(id, name, amount, type)
+        redirect(controller:"recipe", action:"update", id:params.id)
+
+    }
+
+
 
 }
