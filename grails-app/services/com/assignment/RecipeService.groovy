79f04a27b70cf108f8ce1790e30404861c50e548
morantaf <fall_moranta@live.fr>
2020-10-13 10:32:33 +0200

    Add the search logic

--- grails-app/services/com/assignment/RecipeService.groovy
+++ grails-app/services/com/assignment/RecipeService.groovy
@@ -5,19 +5,35 @@ import grails.gorm.transactions.Transactional
 @Transactional
 class RecipeService {
 
-    // Logic to retrieve a Recipe
+    // Retrieve a Recipe
     def get(id) {
         Recipe.get(id)
     }
 
-    // Logic to retrieve the list of recipes
+    // Retrieve the list of recipes
     def list() {
         Recipe.list()
     }
 
-    // Logic to save the recipe
+    // Save the recipe
     def save(recipe) {
         recipe.save()
     }
 
+    // Search a Recipe by its name
+    def search(String query) {
+
+        List<Recipe> listOfRecipes = Recipe.list()
+        def searchedRecipes = []
+
+        // Loop through the list of recipes
+        for(recipe in listOfRecipes) {
+            // Test if the name of the recipe contains or is equal to the query. If so, push the recipe to the list
+            if(recipe.name.equalsIgnoreCase(query) || recipe.name.contains(query)) {
+                searchedRecipes << recipe
+            }
+        }
+        return searchedRecipes
+
+    }
 }
