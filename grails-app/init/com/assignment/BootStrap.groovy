f7db636e4070ffb8c4fd2f0fb7f3236df9bfd0ef
morantaf <fall_moranta@live.fr>
2020-10-13 10:41:48 +0200

    Setting the JSON Marshaller to format the response of the API

--- grails-app/init/com/assignment/BootStrap.groovy
+++ grails-app/init/com/assignment/BootStrap.groovy
@@ -1,9 +1,20 @@
 package com.assignment
 
+import grails.converters.JSON
+
 class BootStrap {
 
     def init = { servletContext ->
-    }
-    def destroy = {
+
+        JSON.registerObjectMarshaller(Recipe) {
+            Recipe recipe ->
+                return [
+                        name: recipe.name,
+                        description: recipe.description,
+                        meal: recipe.meal.name,
+                        ingredients: recipe.ingredients.collect {it.name},
+                        timeToCook: recipe.timeInMinutes
+                ]
+        }
     }
 }
